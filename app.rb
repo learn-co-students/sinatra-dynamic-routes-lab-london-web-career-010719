dbbf3153e9e800ed415cbd1109ce67e3cc48abab
joeyhere <josephodell@outlook.com>
2019-01-28 14:49:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,45 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get "/reversename/:name" do
+    @user_name = params[:name]
+    "#{@user_name.reverse}"
+  end
+
+  get "/square/:number" do
+    "#{params[:number].to_i ** 2}"
+  end
+
+  get "/say/:number/:phrase" do
+    num = params[:number].to_i
+    phrase = ""
+    num.times {phrase = phrase + params[:phrase].to_s}
+    "#{phrase}"
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    add = (params[:number1].to_i + params[:number2].to_i)
+    subtract =  (params[:number1].to_i - params[:number2].to_i)
+    divide = (params[:number1].to_i / params[:number2].to_i)
+    multiply = (params[:number1].to_i * params[:number2].to_i)
+
+    if params[:operation] == "add"
+      "#{add}"
+    elsif params[:operation] == "subtract"
+      "#{subtract}"
+    elsif params[:operation] == "divide"
+      "#{divide}"
+    elsif params[:operation] == "multiply"
+      "#{multiply}"
+    end
+
+  end
+
+
 
 end
